ed6cc0c96a92a2b2bab447a6929aec69402e9b34
SaadJabrane19 <saadjabrane130@gmail.com>
2023-03-11 16:58:24 +0100

    first project

--- saad.py
+++ saad.py
@@ -1,1 +1,14 @@
-print('hello world')
+from random import randint 
+List = randint(1, 1000)
+Running = True
+while Running :
+    user = int(input('enter a number '))
+    if List == user :
+        print('victory')
+        Running = False
+    elif List < user :
+        print('lower')
+    elif List > user : 
+        print('higher')
+
+print('game over')
